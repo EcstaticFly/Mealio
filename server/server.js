48be8e0f9e84c1f1e8060288f7cd65744eeab3cc
Suyash Pandey <suyashpandey607@gmail.com>
2025-09-15 20:29:19 +0530

    Feat(mobile): initialize mobile app with Expo
    Feat(server): setup cron job for periodic API requests
    
    - Added cron.js to configure a job that sends GET requests every 14 minutes (for render deployment).

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { ENV } from './configs/env.js';
 import apiRoutes from './routes/apiRoutes.js';
+import job from './configs/cron.js';
 
 const PORT = ENV.PORT || 5000;
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+if(ENV.NODE_ENV === "production") job.start();
 
 app.get('/', (req,res) => {
     res.send('API is running...');

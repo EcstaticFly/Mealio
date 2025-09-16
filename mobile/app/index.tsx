70f14abfa69cabc8b28fc672387fac123fb4dd00
Suyash Pandey <suyashpandey607@gmail.com>
2025-09-16 22:11:13 +0530

    Feat(mobile): Set up styles, assets for various components and introduce color themes.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +11,9 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/about"}>
+      Click Me
+      </Link>
     </View>
   );
 }

c814defa8f9aa73c3bcc260e8bbe579d15c08585
Suyash Pandey <suyashpandey607@gmail.com>
2025-09-20 23:57:53 +0530

    Feat(auth): Implement SignUp and VerifyEmail screens with form validation and user feedback

--- mobile/app/(auth)/verify-email.jsx
+++ mobile/app/(auth)/verify-email.jsx
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Alert } from "react-native";
+import React, { useState } from "react";
+import { useSignUp } from "@clerk/clerk-expo";
 
-const VerifyEmailScreen = () => {
+const VerifyEmail = ({ email, onBack }) => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleVerification = async () => {
+    if (!isLoaded) return;
+
+    setLoading(true);
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Verification Failed. Please try again.");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (err) {
+      Alert.alert("Error", err.errors?.[0]?.message || "Verification failed");
+      console.error(JSON.stringify(err, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <View>
-      <Text>VerifyEmailScreen</Text>
+      <Text>VerifyEmail</Text>
     </View>
-  )
-}
+  );
+};
 
-export default VerifyEmailScreen
+export default VerifyEmail;
